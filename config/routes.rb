cde050164f71c3207575fbbf7b96b786907474b3
Nicolas Peltier <nicolas.peltier@gmail.com>
2022-08-05 19:26:13 +0200

    relay

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root to: "products#index" # And remove the generated route `products/index`
 end
